75a58177d52b5b47ca8a2aa57e585a369e6dde43
remcostoeten <remcostoeten@hotmail.com>
2023-02-06 22:56:26 +0100

    Tidy up js and add remove function.

--- site.js
+++ site.js
@@ -2,20 +2,16 @@ const count = document.getElementById('count');
 const alert = document.getElementsByClassName('chat__amount');
 const read = document.getElementById('read');
 const wrapper = document.getElementsByClassName('wrapper');
-const amount = document.getElementsByClassName('chat__amount');
-const outer = document.getElementsByClassName('chat__outer');
-read.addEventListener('click', (event) => {
-	const wrapper = document.getElementsByClassName('wrapper');
+const chatAmount = document.getElementsByClassName('chat__amount');
+const chatWrapper = document.getElementsByClassName('chat');
 
-	for (let i = 0; i < alert.length; i++) {
-		alert[i].innerHTML = '0';
-
-		if (amount.textContent === '0') {
-			console.log('✅ if 0');
-			outer[i].classList.add('empty');
-		} else {
-			outer[i].classList.add('not-empty');
-			console.log('else 0');
-		}
+for (let i = 0; i < chatAmount.length; i++) {
+	if (!(chatAmount[i].textContent === '0')) {
+		chatWrapper[i].classList.add('unread');
 	}
-});
+
+	read.addEventListener('click', () => {
+		alert[i].innerHTML = '0';
+		chatWrapper[i].classList.remove('unread');
+	});
+}
