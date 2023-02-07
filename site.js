5df0d27202660c8f6d03bab4c8e625db93831bfb
remcostoeten <remcostoeten@hotmail.com>
2023-02-07 19:35:31 +0100

    Implement some styleguide stuff.

--- site.js
+++ site.js
@@ -7,10 +7,15 @@ for (let i = 0; i < chatAmount.length; i++) {
 	if (!(chatAmount[i].textContent === '0')) {
 		chatWrapper[i].classList.add('unread');
 		alertStar[i].style.display = 'inline-block';
+	} else {
+		chatWrapper[i].classList.add('read');
 	}
 
 	read.addEventListener('click', () => {
 		notificationAlert[i].innerHTML = '0';
 		chatWrapper[i].classList.remove('unread');
+
+		if (chatAmount[i].textContent === '0') {
+		}
 	});
 }
