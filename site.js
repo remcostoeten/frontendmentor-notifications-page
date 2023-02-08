28ad7b2a92fa65bc1fffdb28db1b699e0ce1c8a6
remcostoeten <remcostoeten@hotmail.com>
2023-02-08 14:01:41 +0100

    Add intro.

--- site.js
+++ site.js
@@ -19,3 +19,19 @@ for (let i = 0; i < chatAmount.length; i++) {
 		}
 	});
 }
+
+// intro
+var coll = document.getElementsByClassName('collapsible');
+var i;
+
+for (i = 0; i < coll.length; i++) {
+	coll[i].addEventListener('click', function () {
+		this.classList.toggle('active');
+		var content = this.nextElementSibling;
+		if (content.style.display === 'block') {
+			content.style.display = 'none';
+		} else {
+			content.style.display = 'block';
+		}
+	});
+}
